5bded740500c2310b4767c7687e433b85f6efe83
Hyun-Tae Jin <htjmario@gmail.com>
2023-05-14 15:28:44 -0400

    good enough, form design is done hopefully...

--- src/assets/data/carData.ts
+++ src/assets/data/carData.ts
@@ -24,3 +24,17 @@ export const colors = [
   "White",
   "Yellow",
 ];
+
+// Red = #ff0000 = filter: invert(13%) sepia(90%) saturate(5627%) hue-rotate(3deg) brightness(98%) contrast(119%);
+// Black = #000000 = filter: invert(0%) sepia(0%) saturate(0%) hue-rotate(324deg) brightness(100%) contrast(100%);
+// Blue = #0000ff = filter: invert(8%) sepia(98%) saturate(7357%) hue-rotate(248deg) brightness(93%) contrast(145%);
+// Brown = #a52a2a = filter: invert(16%) sepia(42%) saturate(5363%) hue-rotate(347deg) brightness(96%) contrast(83%);
+// Gold = #ffd700 = filter: invert(82%) sepia(14%) saturate(6279%) hue-rotate(1deg) brightness(109%) contrast(104%);
+// Gray = #808080 = filter: invert(52%) sepia(0%) saturate(0%) hue-rotate(235deg) brightness(97%) contrast(96%);
+// Green = #008000 = filter: invert(24%) sepia(98%) saturate(1677%) hue-rotate(99deg) brightness(98%) contrast(101%);
+// Orange = #ffa500 = filter: invert(65%) sepia(37%) saturate(2512%) hue-rotate(359deg) brightness(102%) contrast(105%);
+// Purple = #800080 = filter: invert(9%) sepia(94%) saturate(5113%) hue-rotate(295deg) brightness(83%) contrast(110%);
+// Silver = #c0c0c0 = filter: invert(84%) sepia(15%) saturate(0%) hue-rotate(213deg) brightness(91%) contrast(90%);
+// Tan = #d2b48c = filter: invert(82%) sepia(29%) saturate(387%) hue-rotate(351deg) brightness(87%) contrast(86%);
+// White = #ffffff = filter: invert(100%) sepia(0%) saturate(7500%) hue-rotate(24deg) brightness(104%) contrast(105%);
+// Yellow = #ffff00 = filter: invert(90%) sepia(92%) saturate(1428%) hue-rotate(358deg) brightness(104%) contrast(107%);
--- src/components/CarForm/CarForm.tsx
+++ src/components/CarForm/CarForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { useDispatch, useStore } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import {
   getMake,
@@ -51,10 +51,10 @@ interface CarFormProps {
 
 export const CarForm = (props: CarFormProps) => {
   const dispatch = useDispatch();
-  const store = useStore();
   const { register, handleSubmit } = useForm({});
   const { ref, ...registerProps } = register("price");
   const comboboxRef = useRef<HTMLInputElement>(null);
+  const [stage, setStage] = useState(0);
 
   const [selectedMake, setSelectedMake] = useState<{
     id: number;
@@ -64,26 +64,34 @@ export const CarForm = (props: CarFormProps) => {
     id: number;
     name: string;
   } | null>(null);
-
   const [selectedYear, setSelectedYear] = useState(null);
   const [selectedVehicleType, setSelectedVehicleType] = useState<{
     id: string;
     name: string;
   } | null>(null);
-  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedColor, setSelectedColor] = useState<{
+    id: number;
+    name: keyof typeof colorFilters;
+  } | null>(null);
   const [priceInput, setPriceInput] = useState("");
   const [isNew, setIsNew] = useState(false);
 
   const handleMakeChange = (value: any) => {
     setSelectedMake(value);
+    if (value) setStage(1);
+    else resetStagesFrom(0);
   };
 
   const handleModelChange = (value: any) => {
     setSelectedModel(value);
+    if (value) setStage(2);
+    else resetStagesFrom(1);
   };
 
   const handleYearChange = (value: any) => {
     setSelectedYear(value);
+    if (value) setStage(3);
+    else resetStagesFrom(2);
   };
 
   const handleVehicleTypeChange = (value: any) => {
@@ -93,10 +101,51 @@ export const CarForm = (props: CarFormProps) => {
     } else {
       setSelectedVehicleType(null);
     }
+    if (value) setStage(4);
+    else resetStagesFrom(3);
   };
 
   const handleColorChange = (value: any) => {
     setSelectedColor(value);
+    if (value) setStage(5);
+    else resetStagesFrom(4);
+  };
+
+  const resetStagesFrom = (fromStage: number) => {
+    if (fromStage <= 0) setSelectedMake(null);
+    if (fromStage <= 1) setSelectedModel(null);
+    if (fromStage <= 2) setSelectedYear(null);
+    if (fromStage <= 3) setSelectedVehicleType(null);
+    if (fromStage <= 4) setSelectedColor(null);
+    if (fromStage <= 5) {
+      setPriceInput("");
+      setIsNew(false);
+    }
+    setStage(fromStage);
+  };
+
+  const colorFilters = {
+    Black:
+      "invert(0%) sepia(0%) saturate(0%) hue-rotate(324deg) brightness(100%) contrast(100%)",
+    Blue: "invert(8%) sepia(98%) saturate(7357%) hue-rotate(248deg) brightness(93%) contrast(145%)",
+    Brown:
+      "invert(16%) sepia(42%) saturate(5363%) hue-rotate(347deg) brightness(96%) contrast(83%)",
+    Gold: "invert(82%) sepia(14%) saturate(6279%) hue-rotate(1deg) brightness(109%) contrast(104%)",
+    Gray: "invert(52%) sepia(0%) saturate(0%) hue-rotate(235deg) brightness(97%) contrast(96%)",
+    Green:
+      "invert(24%) sepia(98%) saturate(1677%) hue-rotate(99deg) brightness(98%) contrast(101%)",
+    Orange:
+      "invert(65%) sepia(37%) saturate(2512%) hue-rotate(359deg) brightness(102%) contrast(105%)",
+    Purple:
+      "invert(9%) sepia(94%) saturate(5113%) hue-rotate(295deg) brightness(83%) contrast(110%)",
+    Red: "invert(13%) sepia(90%) saturate(5627%) hue-rotate(3deg) brightness(98%) contrast(119%)",
+    Silver:
+      "invert(84%) sepia(15%) saturate(0%) hue-rotate(213deg) brightness(91%) contrast(90%)",
+    Tan: "invert(82%) sepia(29%) saturate(387%) hue-rotate(351deg) brightness(87%) contrast(86%)",
+    White:
+      "invert(100%) sepia(0%) saturate(7500%) hue-rotate(24deg) brightness(104%) contrast(105%)",
+    Yellow:
+      "invert(90%) sepia(92%) saturate(1428%) hue-rotate(358deg) brightness(104%) contrast(107%)",
   };
 
   const bodyTypes = [
@@ -158,7 +207,7 @@ export const CarForm = (props: CarFormProps) => {
       dispatch(getVehicleType(data.vehicle_type));
       dispatch(getFuelType(data.fuel_type));
       dispatch(getImage(data.image));
-      await serverCalls.create(store.getState());
+      await serverCalls.create(data);
       window.location.reload();
     }
     const currentYear = new Date().getFullYear();
@@ -176,25 +225,27 @@ export const CarForm = (props: CarFormProps) => {
         <Combobox
           {...register("make")}
           name="make"
-          placeholder="Select a make"
+          placeholder="Toyota"
           items={makes.map((make, index) => ({ id: index, name: make }))}
           selectedItem={selectedMake}
           onChange={handleMakeChange}
           className="z-30 mr-12 w-[120px]"
+          disabled={stage < 0}
         />
         <Combobox
           {...register("model")}
           name="model"
-          placeholder="Select a model"
+          placeholder="Corolla"
           items={getModels()}
           selectedItem={selectedModel}
           onChange={handleModelChange}
           className="mr-12 w-[15em]"
+          disabled={stage < 1}
         />
         <Combobox
           {...register("year")}
           name="year"
-          placeholder="Select a year"
+          placeholder="2022"
           items={years.map((year, index) => ({
             id: index,
             name: year.toString(),
@@ -202,6 +253,7 @@ export const CarForm = (props: CarFormProps) => {
           selectedItem={selectedYear}
           onChange={handleYearChange}
           className="w-[3.5em]"
+          disabled={stage < 2}
         />
       </div>
       <div className="mx-auto flex w-[545px] items-center justify-center">
@@ -209,7 +261,7 @@ export const CarForm = (props: CarFormProps) => {
           <Combobox
             ref={comboboxRef}
             name="vehicle_type"
-            placeholder="Select a vehicle_type"
+            placeholder="Sedan"
             items={vehicle_types.map((vehicle_type, index) => ({
               id: index,
               name: vehicle_type,
@@ -224,11 +276,12 @@ export const CarForm = (props: CarFormProps) => {
             }
             onChange={handleVehicleTypeChange}
             className="z-20 my-4 w-full"
+            disabled={stage < 3}
           />
           <Combobox
             {...register("color")}
             name="color"
-            placeholder="Select a color"
+            placeholder="Blue"
             items={colors.map((color, index) => ({
               id: index,
               name: color,
@@ -236,8 +289,13 @@ export const CarForm = (props: CarFormProps) => {
             selectedItem={selectedColor}
             onChange={handleColorChange}
             className="z-10 w-full"
+            disabled={stage < 4}
           />
-          <div className="z-0 mb-8 mt-4 flex w-[120px] -translate-y-2 translate-x-[2px]">
+          <div
+            className={`translate-x-[2px]} z-0 mb-8 mt-4 flex w-[120px] -translate-y-2 ${
+              stage < 5 ? "pointer-events-none opacity-40" : ""
+            }`}
+          >
             <div className="flex -translate-x-4 flex-col">
               <div className="flex -translate-x-6 translate-y-0.5 items-center">
                 <span className="-translate-x-0.5 translate-y-7">
@@ -254,8 +312,9 @@ export const CarForm = (props: CarFormProps) => {
                 value={priceInput === "0.00" ? "" : priceInput}
                 placeholder="0.00"
                 ref={ref}
-                className="hover:-ring-offset-1 w-[75px] rounded-lg border-0 bg-slate-800 py-1.5 pl-4 text-[var(--text-color)] shadow-sm ring-1 ring-inset ring-violet-500 hover:ring-1 hover:ring-[var(--hover-color)] focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="hover:-ring-offset-1 w-[75px] rounded-lg border-0 bg-slate-800 py-1.5 pl-4 text-right text-[var(--text-color)] shadow-sm ring-1 ring-inset ring-violet-500 hover:ring-1 hover:ring-[var(--hover-color)] focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 onChange={handlePriceInput}
+                disabled={stage < 5}
               />
             </div>
             <div className="flex -translate-x-[0.5em] translate-y-0.5 flex-col">
@@ -269,11 +328,16 @@ export const CarForm = (props: CarFormProps) => {
                 type="checkbox"
                 checked={isNew}
                 onChange={(e) => setIsNew(e.target.checked)}
+                disabled={stage < 5}
               />
             </div>
           </div>
         </div>
-        <div className="relative ml-4 mt-2 grid h-[13.75em] w-full scale-90 cursor-pointer select-none grid-cols-3 grid-rows-3 flex-col place-items-center items-end gap-2 rounded-2xl border-2 border-dashed border-indigo-400 p-2">
+        <div
+          className={`relative ml-4 mt-2 grid h-[13.75em] w-full scale-90 cursor-pointer select-none grid-cols-3 grid-rows-3 flex-col place-items-center items-end gap-2 rounded-2xl border-2 border-dashed border-indigo-400 p-2 ${
+            stage < 4 ? "pointer-events-none opacity-40" : ""
+          }`}
+        >
           {bodyTypes.map(({ type, image, width }, index) => {
             const isSelected =
               selectedVehicleType && selectedVehicleType.name === type;
@@ -293,7 +357,7 @@ export const CarForm = (props: CarFormProps) => {
                   if (isSelected) {
                     setSelectedVehicleType(null);
                   } else {
-                    const vehicleType = vehicle_types[index]; // get the reference directly from the array
+                    const vehicleType = vehicle_types[index];
                     handleVehicleTypeChange({
                       id: index.toString(),
                       name: vehicleType,
@@ -306,14 +370,19 @@ export const CarForm = (props: CarFormProps) => {
                       comboboxRef.current.focus();
                     }
                   }
-                  console.log(selectedVehicleType)
+                  console.log(selectedVehicleType);
                 }}
               >
                 <img
                   src={image}
                   alt={type}
                   className="h-fit object-cover"
-                  style={{ width: `${width}px` }}
+                  style={{
+                    width: `${width}px`,
+                    filter: selectedColor
+                      ? `grayscale(100%) ${colorFilters[selectedColor.name]}`
+                      : "none",
+                  }}
                 />
               </div>
             );
